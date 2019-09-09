5eb7c27b02170f2277a958f3cb38c234c2fa9640
Michał Białecki <miszekb@gmail.com>
2019-09-09 18:53:30 +0200

    [ADD] project initial boilerplate

---
+++ hello-world/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    <div class="nav">
+      <router-link to="/">spacer</router-link>
+      <router-link to="/about">about</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
